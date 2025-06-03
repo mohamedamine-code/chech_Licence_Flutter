aa14bb9c3eb82c1b0976c64c9c5828064c13b8c8
mohamedamine-code <mohamedaminebensir09@gmail.com>
2025-06-03 11:37:57 +0100

    intial commit

---
+++ android/app/src/main/kotlin/com/example/check_license/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.check_license
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:check_license/pages/HomePage.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Homepage(),
+    );
+  }
+}
