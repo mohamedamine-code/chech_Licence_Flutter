e09e80c6df2ca88616b70d3f8d678ae38036e767
mohamedamine-code <mohamedaminebensir09@gmail.com>
2025-06-10 19:18:08 +0100

    some modifiction  commit

--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,8 @@ import 'package:check_license/pages/AddLicense.dart';
 import 'package:check_license/pages/ViewArchiv.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
 import 'package:provider/provider.dart';
-
 import 'firebase_options.dart';
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
